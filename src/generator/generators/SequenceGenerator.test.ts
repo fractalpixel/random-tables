4f579d304fea285a95ed659c4a54a4b7aee81873
fractalpixel <n/a>
2024-11-29 00:18:51 +0200

    Added test cases for empty sequences and tables.

--- src/generator/generators/SequenceGenerator.test.ts
+++ src/generator/generators/SequenceGenerator.test.ts
@@ -3,6 +3,11 @@ import ConstantGenerator from "./ConstantGenerator";
 import SequenceGenerator from "./SequenceGenerator";
 
 test("Sequence generator", () => {
+
+    const g0 = new SequenceGenerator([])
+    expect(g0.generate()).toEqual("")
+
     const g = new SequenceGenerator([new ConstantGenerator("foo"), new ConstantGenerator("bar")])
     expect(g.generate()).toEqual("foobar")
+
 })
--- src/generator/generators/TableGenerator.test.ts
+++ src/generator/generators/TableGenerator.test.ts
@@ -4,8 +4,14 @@ import ConstantGenerator from "./ConstantGenerator";
 import MockRandom from "src/random/MockRandom";
 
 test("Table generator", () => {
-    const g = new TableGenerator([new ConstantGenerator("foo"), new ConstantGenerator("bar")])
     const rng = new MockRandom(0)
+
+    const g0 = new TableGenerator([])
+    expect(g0.generate(rng)).toEqual("")
+
+    const g = new TableGenerator([new ConstantGenerator("foo"), new ConstantGenerator("bar")])
+
+    rng.randomValue = 0
     expect(g.generate(rng)).toEqual("foo")
 
     rng.randomValue = 1
